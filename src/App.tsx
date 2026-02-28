66d5edc0bd309354cbf59962000ed986aff27d78
zbabiarz <111098143+zbabiarz@users.noreply.github.com>
2026-02-27 17:42:31 -0700

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -133,10 +133,10 @@ export default function App() {
                 <div className="relative w-full" style={{ paddingBottom: '56.25%' }}>
                   <iframe
                     className="absolute top-0 left-0 w-full h-full rounded-lg"
-                    src="https://www.youtube.com/embed/kcf6tsou5Us"
+                    src="https://player.vimeo.com/video/1169022608"
                     title="Platform Overview Video"
                     frameBorder="0"
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allow="autoplay; fullscreen; picture-in-picture"
                     allowFullScreen
                   />
                 </div>
